788c36f31f0ef4cbeff7a7d7b4ec2e419b150718
Nikhil M R <nkhlrgh@gmail.com>
2021-04-05 15:58:46 +0530

    feat: Add converted value on select

--- app.js
+++ app.js
@@ -4,17 +4,26 @@ var url = "https://v6.exchangerate-api.com/v6/8bd5a86627129321d9e69b79/latest/";
 
 var baseInput = document.querySelector("#base-currency");
 
+var convertInput = document.querySelector("#convert-currency");
+
 function constructUrl(inp) {
   return url + inp;
 }
 
+function con() {
+  return baseInput.value;
+}
+
 function changeHandler() {
-  var baseCurrency = baseInput.value;
+  var baseCurrency = con();
+  var final = convertInput.value;
   fetch(constructUrl(baseCurrency))
     .then((response) => response.json())
     .then((json) => {
-      console.log(json.conversion_rates.INR);
+      console.log(json.conversion_rates[final]);
     });
 }
 
-baseInput.addEventListener("change", changeHandler);
+baseInput.addEventListener("change", con);
+
+convertInput.addEventListener("change", changeHandler);
